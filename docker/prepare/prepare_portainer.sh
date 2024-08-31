ba4c291a79b5a73b5fff3e2090126b8099497475
Rainman1980X <R@inman123>
2024-08-31 18:11:28 +0200

    Initialize Portainer admin user in prepare script
    Add script to create initial admin user for Portainer with default credentials after container startup, including error handling for the API response.

--- docker/prepare/prepare_portainer.sh
+++ docker/prepare/prepare_portainer.sh
@@ -14,3 +14,24 @@ docker-compose -f "$PORTAINER_DIR/docker-compose.yml" up -d portainer
 
 printf "Waiting for 15 sec to start up...\n"
 sleep 15
+
+printf "Init adminuser for portainer\n"
+
+PAYLOAD=$(jq -n '{
+      "username": "admin",
+      "password": "admin1234567890"
+    }')
+
+RESPONSE=$(curl -s -X POST "http://localhost:9000/api/users/admin/init" \
+    -H "Content-Type: application/json" \
+    -d "$PAYLOAD")
+
+if printf "Response %s" "$RESPONSE" | grep -q "message"; then
+  message=$(echo "$RESPONSE" | jq -r .message)
+  details=$(echo "$RESPONSE" | jq -r .details)
+
+  printf 'Error creating the admin account \n{\n"message":"%s",\n"details":"%s"\n}\n' "$message" "$details"
+
+else
+  printf "Admin account created successfully\n"
+fi
