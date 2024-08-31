00416f0c910353d45d9ba6812c0bb02ac1e875c1
Rainman1980X <R@inman123>
2024-08-31 11:51:52 +0200

    Add Portainer setup and update upload script
    Introduce a new docker-compose file for Portainer in the preparation directory. Modify `upload_all_stacks.sh` to update and start Portainer, and handle the existence of existing stacks by deleting them if present. Additionally, move the Nexus compose file to a new directory for better organization.

--- src/upload_all_stacks.sh
+++ src/upload_all_stacks.sh
@@ -4,7 +4,17 @@
 PORTAINER_URL="http://localhost:9000"
 USERNAME="admin"
 PASSWORD="admin1234567890"
-BASE_DIR="../docker"  # Basisverzeichnis für alle docker-compose-Dateien
+BASE_DIR="../docker/compose"  # Basisverzeichnis für alle docker-compose-Dateien
+PORTAINER_DIR="./docker/prepare/portainer"
+
+# Aktualisieren und Starten von Portainer
+echo "Updating and starting Portainer..."
+docker system prune -a --volumes -f
+docker-compose down
+docker-compose -f "$PORTAINER_DIR/docker-compose.yml" pull portainer
+docker-compose -f "$PORTAINER_DIR/docker-compose.yml" up -d portainer
+
+sleep 15
 
 # Get JWT Token
 echo "Authenticating and retrieving JWT token..."
@@ -45,6 +55,20 @@ for APP_DIR in "$BASE_DIR"/*/; do
     ESCAPED_COMPOSE_FILE_CONTENT=$(echo "$COMPOSE_FILE_CONTENT" | jq -R -s '.')
 
     echo "Uploading docker-compose.yml from $APP_DIR to Portainer..."
+
+    echo "Checking if stack already exists..."
+
+    # check if stack already exists
+    EXISTING_STACK_ID=$(curl -s -H "Authorization: Bearer $JWT_TOKEN" "$PORTAINER_URL/api/stacks" | jq -r ".[] | select(.Name == \"${STACK_NAME}\") | .Id")
+
+    if [ "$EXISTING_STACK_ID" ]; then
+      echo "Stack already exists. Deleting old stack..."
+
+      # delete existing stack
+      curl -s -X DELETE -H "Authorization: Bearer $JWT_TOKEN" "$PORTAINER_URL/api/stacks/$EXISTING_STACK_ID"
+    else
+      echo "Stack does not exist. Proceeding with creation of new stack..."
+    fi
 
     # Properly escape newlines for JSON compatibility
     PAYLOAD=$(jq -n --arg name "$STACK_NAME" --arg stackFileContent "$COMPOSE_FILE_CONTENT" '{
