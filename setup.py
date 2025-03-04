2aa04e5d4ec1dab1c030c422943faf9e8d3c467f
Rainman1980X <R@inman123>
2025-03-04 17:13:22 +0100

    Update package directory to "docker" in setup.py
    Changed the `find_packages` argument in `setup.py` from `"."` to `"docker"`. This ensures that the packaging process correctly identifies the desired modules within the "docker" directory. No other changes were made.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="infrastructure",
     version="0.1",
-    packages=find_packages(where="."),
+    packages=find_packages(where="docker"),
     package_dir={"": "."},
     include_package_data=True,
     install_requires=[],  # Optional: requirements.txt lesen lassen
