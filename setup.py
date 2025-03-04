bdcfbe1aa560a153976e1b44e2dcf2c250c55b13
Rainman1980X <R@inman123>
2025-03-04 17:09:24 +0100

    Add `setup.py` and update dependencies and CI workflow.
    Introduce `setup.py` for package configuration and editable installs. Update `requirements.txt` with revised dependency versions. Modify GitHub Actions workflow to use Python 3.12, adjust dependency installation steps, and replace pytest with unittest for test execution.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="infrastructure",
+    version="0.1",
+    packages=find_packages(where="."),
+    package_dir={"": "."},
+    include_package_data=True,
+    install_requires=[],  # Optional: requirements.txt lesen lassen
+)
