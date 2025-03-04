bcd6bd143ebb9134a902ec0c0893e1a519c5d6c1
Rainman1980X <R@inman123>
2025-03-04 17:25:38 +0100

    Update package configuration and PYTHONPATH setup
    Adjust `package_dir` in setup.py to align with module structure and modify PYTHONPATH in the GitHub workflow to include the correct `docker` directory. These changes ensure proper package discovery and test execution.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="infrastructure",
     version="0.1",
-    packages=find_packages(where="docker"),  # Sucht nur in docker/
-    package_dir={"": "docker"},  # Sagt setuptools, dass docker das Root-Modul ist
+    packages=find_packages(where="docker"),
+    package_dir={"infrastructure": "docker"},
     include_package_data=True,
     install_requires=[],
 )
