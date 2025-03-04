8070c950859bd6a123044cc2dd37641a1ba07394
Rainman1980X <R@inman123>
2025-03-04 18:18:53 +0100

    Update package directory and simplify PYTHONPATH setup
    Changed the package search directory in `setup.py` to `docker` and updated `package_dir` accordingly. Simplified the PYTHONPATH setup in the GitHub Actions workflow by removing redundant path manipulation.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="infra-core",
     version="0.1",
-    packages=find_packages(where="."),  # Suche in allen Verzeichnissen
-    package_dir={"": "."},  # Setze das Root-Verzeichnis als Basis
+    packages=find_packages(where="docker"),
+    package_dir={"": "docker"},
     include_package_data=True,
     install_requires=[],
 )
